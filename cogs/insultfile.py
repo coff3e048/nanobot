72e1f77da7e2048ea30da33f1ec6f423b882d84d
Nathan Ritchie <48845980+ctaetcsh@users.noreply.github.com>
2021-12-13 22:50:41 -0800

    Add insults

--- cogs/insultfile.py
+++ cogs/insultfile.py
@@ -11,5 +11,8 @@ class insult_list():
         "canada is not a real country",
         "eat my nuts you sad sack of shit",
         "go fuck yourself",
-        "get off discord and go outside"
+        "get off discord and go outside",
+        "Where's your tomboy tradwife, huh?",
+        "You're being more faggoty then ctaetcsh rn",
+        "Are you ran because your computer is shit"
     ]
