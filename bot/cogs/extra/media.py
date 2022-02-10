c1220b533f7b6a34bd13d935ee766a93adbf83c8
coff3e048 <jamesburns64@protonmail.com>
2022-02-10 18:56:15 +0000

    Merge media-related cogs into one cog

--- bot/cogs/extra/media.py
+++ bot/cogs/extra/media.py
@@ -3,19 +3,48 @@ import os
 import subprocess
 import discord
 import asyncio
+import re
+import aiohttp
 import aiofiles.os
 from nextcord.ext import commands
 from env_var import env
 
 
-class ytdlcmd(commands.Cog):
+class mediastuff(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.session = aiohttp.ClientSession()
 
-    @commands.command(name="ytdl", alias=["youtubedl"])
+    async def _youtube_results(self, query: str):
+        try:
+            headers = {"user-agent": "nanobot/3.0"}
+            async with self.session.get(
+                "https://www.youtube.com/results", params={"search_query": query}, headers=headers
+            ) as r:
+                result = await r.text()
+            yt_find = re.findall(r"{\"videoId\":\"(.{11})", result)
+            url_list = []
+            for track in yt_find:
+                url = f"https://www.youtube.com/watch?v={track}"
+                if url not in url_list:
+                    url_list.append(url)
+        except Exception as e:
+            url_list = [f"Something went terribly wrong! [{e}]"]
+        return url_list
+
+    @commands.command(name="youtube", alias=["yt"])
+    async def youtube(self, ctx, *, query: str):
+        """Search on Youtube."""
+        result = await self._youtube_results(query)
+        if result:
+            await ctx.reply(result[0])
+        else:
+            await ctx.reply("Nothing found. Try again later.")
+
+    @commands.command(name="ytdl", alias=["youtubedl", "ydl"])
     @commands.cooldown(1, 10, commands.BucketType.user)
-    async def yt_dl(self, ctx, query: str = None, convert: str = "video"):
+    async def ytdl(self, ctx, query: str = None, convert: str = "video"):
 
         # Example video(s): http://www.youtube.com/watch?v=BaW_jenozKc
         #                   http://www.youtube.com/watch?v=jNQXAC9IVRw
@@ -27,9 +56,8 @@ class ytdlcmd(commands.Cog):
             info_dict = ydl.extract_info(query, download=False)
             video_id = info_dict.get("id", None)
 
-        dlpath = "'.temp/%(id)s.%(ext)s'"
+        dlpath = "'temp/%(id)s.%(ext)s'"
         usage = f"{env.prefix}ytdl 'https://youtube.com/...' video / audio (default: video)"
-
         if not query:
             await ctx.reply(f"No YouTube URL found.\n```{usage}```")
         else:
@@ -77,8 +105,18 @@ class ytdlcmd(commands.Cog):
                 await msg.reply(f"Something went wrong. ```{exitcode}```")
             # remove the file after a certain amount of time (in this case, 14400 seconds is 4 hours)
             await asyncio.sleep(14400)
-            await aiof.os.remove(f"temp/{vidfile}")
+            if os.path.exists(f"temp/{vidfile}"):
+                await aiof.os.remove(f"temp/{vidfile}")
+
+    @commands.command(name="animalfact")
+    async def animal_fact(self, ctx: commands.Context, URL="https://some-random-api.ml/animal/"):
+        async with request("GET", URL, headers={}) as response:
+            if response.status == 200:
+                data = await response.json()
+                await ctx.reply(data["fact"])
+            else:
+                await ctx.reply(f"API returned a {response.status} status")
 
 
 def setup(bot: commands.Bot):
-    bot.add_cog(ytdlcmd(bot))
+    bot.add_cog(mediastuff(bot))
