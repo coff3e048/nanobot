44937eb0424a8188552a32bcbb72b98cfda433d2
coff3e048 <jamesburns64@protonmail.com>
2022-02-10 21:28:01 +0000

    comments and better formatting

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,4 @@
-print("Importing packages")
+print("Importing modules")
 try:
     import time
     # Log the amount of time it takes to start the bot
@@ -28,6 +28,8 @@ version = env.version
 sourcepage = env.sourcepage
 prefix = env.prefix
 
+
+# OS info + check
 console.system(f"System:\t")
 inspect(platform.uname())
 osplatform = platform.system()
@@ -37,31 +39,34 @@ if osplatform != "Linux":
     )
 
 
-if env.activitytype == 'playing':
-    activitytype = discord.ActivityType.playing
-elif env.activitytype == 'watching':
-    activitytype = discord.ActivityType.watching
-elif env.activitytype == 'listening':
-    activitytype = discord.ActivityType.listening
+# Bot profile status type (playing..., watching..., listening...)
+if env.statustype == 'playing':
+    statustype = discord.ActivityType.playing
+elif env.statustype == 'watching':
+    statustype = discord.ActivityType.watching
+elif env.statustype == 'listening':
+    statustype = discord.ActivityType.listening
 
-
+# Try intents, if it's not enabled on the dev page then complain.
 intents = discord.Intents.default()
-intents.members = True
-intents.guilds = True
-activity = discord.Activity(name=env.botactivity, type=activitytype)
+try:
+    intents.members = True
+    intents.guilds = True
+except Exception as e:
+    console.error(e)
+activity = discord.Activity(name=env.botstatus, type=statustype)
 
 
-console.log(
-    f"Starting nanobot ({name})"
-)
+console.log(f"Starting nanobot ({name})")
+connect_start_time = time.time()
 
 
 # Bot params + info
 bot = commands.Bot(
     command_prefix=prefix,
-    case_insensitive=True,
+    case_insensitive=True,  # Be able to take any letter case
     intents=intents,
-    help_command=None,
+    help_command=None,      # Custom help command
     activity=activity,
     allowed_mentions=discord.AllowedMentions(
         users=True,         # Whether to ping individual user @mentions
@@ -72,12 +77,20 @@ bot = commands.Bot(
 )
 
 
-def returnln(returns: int = 1):
+# Convenience function, find a safer way than using sys
+def returnln(returns=1):
     for x in range(returns):
-        ln = sys.stdout.write("\033[F")
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K")
 
 
+def timed(_end_time, _start_time, x):
+    return round((_end_time - _start_time) * x)
+
+
+# Load bot cogs in bot/cogs
 def cogservice(filepath):
+    # These cogs are fallback cogs, in the event that no service file is present the bot will instead load these.
     basic_cogs = ['errorhandler',
                   'botmgr',
                   'help',
@@ -87,11 +100,12 @@ def cogservice(filepath):
                   ]
     console.log(f"Finding {filepath}")
     returnln()
-    if os.path.exists(filepath):
+    if os.path.exists(filepath):    # filepath is defined in the function call
         console.success(f"Found {filepath}")
         with open(filepath, 'r') as service:
             cogsenabled = service.read().split()
     else:
+        # Ask if the user wants to create a service file with the basic fallback cogs
         cogsenabled = basic_cogs
         console.error(
             f"'{filepath}' wasn't found. Create the file with basic cogs? (Y/n)")
@@ -104,18 +118,19 @@ def cogservice(filepath):
                 f.write(f"{cogs}\n")
     service.close()
     for cogs in cogsenabled:
+        # Start loading cogs
         try:
             cog_start_time = time.time()
             console.log(f"loading {cogs}")
             bot.load_extension(cogs)
             cog_end_time = time.time()
-            # time.sleep(2)
             returnln()
             console.success(
-                f"loaded {cogs} ({round((cog_end_time - cog_start_time) * 1000)}ms)")
+                f"loaded {cogs} (took {timed(cog_end_time,cog_start_time,1000)}ms)")
         except Exception as e:
-            sys.stdout.write("\033[F")
-            console.error(f"loading {cogs} failed:\n({e})")
+            returnln()
+            console.error(
+                f"loading {cogs} failed:\n({e} (took {timed(cog_end_time,cog_start_time,1000)}ms)")
 
 
 async def botupdate(URL: str = "https://raw.githubusercontent.com/get-coff3e/nanobot/testing/bot/version.json"):
@@ -125,15 +140,18 @@ async def botupdate(URL: str = "https://raw.githubusercontent.com/get-coff3e/nan
                 data = await response.json(content_type=None)
                 newversion = data["botversion"]
                 if newversion != version:
-                    print(
+                    console.notice(
                         f"Newest nanobot version on github doesn't match the one installed. It may be outdated, please consider updating.\n\t\t\tQueried URL:\t\t{URL}\n\t\t\tInstance Version:\t{ version}\n\t\t\tLatest found:\t\t{newversion}\n\t\t\tIf you would like to disable this message, set [bold orange]UPDATEMSG[/] to False in your environment")
             else:
-                print("Couldn't reach github")
+                print(
+                    f"Couldn't reach github to check for updates. HTTP Response: {response.status}")
 
 
 @bot.event
 async def on_ready():
-    console.success('Connection made!\n')
+    connect_end_time = time.time()
+    console.success(
+        f'Connected to Discord API! (took {timed(connect_end_time,connect_start_time,1000)}ms)\n')
     # nanobot startup ascii art
     console.nanostyle(
         text2art(name, 'random')
@@ -147,23 +165,22 @@ async def on_ready():
         print(f"[white]\nVersion: {version}[/]")
 
 # Discord API User info + prefix
-    print("\n----------------------------------------")
+    print("\n\t----------------------------------------")
     print()
-    print(f'[magenta]Logged in as [/][underline]{bot.user}[/] ({bot.user.id})')
-    print(f'[magenta]Prefix: {prefix}[/]')
+    print(f'\t[magenta]Logged in as [/][underline]{bot.user}[/] ({bot.user.id})')
+    print(f'\t[magenta]Prefix: {prefix}[/]')
     print()
-    print("----------------------------------------\n")
+    print("\t----------------------------------------\n")
 
-# printing list of joined guilds and its data
+# printing list of guilds the bot is joined into
     console.botlog("Joined guilds:")
-    num = 0
     joined = bot.guilds
     count = []
     if len(joined) > 1:
         for guilds in joined:
             try:
                 count.append(guilds)
-                print(f'\t\t\t\t{len(count)} - {guilds}')
+                print(f'\t\t\t\t{len(count)} -\t{guilds}')
             except Exception as e:
                 console.error(e)
         console.botlog(f'Total joined guilds: {len(joined)}')
@@ -174,19 +191,22 @@ async def on_ready():
         users = bot.users
 # Uncomment these lines below to print out all users the bot sees. Otherwise, it will only show you how many there are.
         #console.botlog(f"Found users:")
-        # try:
-        #    print(users)
-        # except Exception as e:
-        #    console.error(e)
+        #count = []
+        # for member in users:
+        #    try:
+        #        count.append(member)
+        #        print(f'\t\t\t\t{len(count)} -\t{member}')
+        #   except Exception as e:
+        #       console.error(e)
         console.botlog(f"Total unique users found: {len(users)}")
 
 # load cogs from cogservice function
     cogservice('config/service.txt')
 
-# printing the time it took to start the bot
+# print the time it took to start the bot
     end_time = time.time()
     console.log(
-        f"Took {round((end_time - start_time) * 1000)}ms ({round((end_time - start_time) * 1)}s) to start-up"
+        f"Took {timed(end_time,start_time,1000)}ms ({timed(end_time,start_time,1)}s) to start-up"
     )
 
     await botupdate()
