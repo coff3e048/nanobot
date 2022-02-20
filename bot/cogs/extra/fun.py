a188cd2b7c0a23a143b9e675da43679ce75e5309
get-coff3e <64768219+get-coff3e@users.noreply.github.com>
2022-02-20 02:00:29 -0600

    more fun!!11!

--- bot/cogs/extra/fun.py
+++ bot/cogs/extra/fun.py
@@ -2,7 +2,6 @@ import random
 import asyncio
 import os
 import discord
-import numpy.random
 from nextcord.ext import commands
 from art import text2art
 
@@ -17,15 +16,15 @@ class Fun(commands.Cog):
     async def roll(self, ctx, first: int = 1, second: int = 100):
         await ctx.reply(f"{random.randint(first,second)} *({first} - **{second}**)*")
 
-    @commands.command(name="choose")
-    async def choose(self, ctx: commands.Context, *, text: str = None):
+    @commands.command(name="choose", aliases=['pick'])
+    async def choose(self, ctx, *, text: str = None):
         if text:
             await ctx.reply(random.choice(text.split()))
         else:
             await ctx.reply(f"You've given me nothing to chose between.")
 
-    @commands.command(name="8ball")
-    async def eightball(self, ctx: commands.Context):
+    @commands.command(name="8ball", aliases=['8-ball', '8b', '8'])
+    async def eightball(self, ctx):
         responses = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
                      "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
                      "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.",
@@ -33,35 +32,35 @@ class Fun(commands.Cog):
         msg = await ctx.reply(':8ball: *Thinking*')
         choice = random.choice(responses)
         await asyncio.sleep(1)
-        # i want to keep this cause its really funny
-        #await msg.edit(':8ball: '+"...".join(choice))
+        # i want to keep this cause its funny
+        # await msg.edit(':8ball: '+"...".join(choice))
         await msg.edit(f':8ball: {choice}')
 
     @commands.command(name="ascii")
     @commands.cooldown(1, 1, commands.BucketType.user)
-    async def ascii(self, ctx: commands.Context, *, text: str = "Hello World!"):
+    async def ascii(self, ctx, *, text: str = "Hello World!"):
         textart = text2art(text, 'random')
-        file = f'ascii-{ctx.author.id}.txt'
         try:
             await ctx.reply(f"```{textart}```")
         except:
             data = io.BytesIO(textart)
-            await ctx.reply(file=discord.File(data, file))
+            await ctx.reply(file=discord.File(data, f'ascii-{ctx.author.id}.txt'))
 
     @commands.command(name="battle")
-    @commands.cooldown(1, 1, commands.BucketType.user)
-    async def auto_battle(self, ctx: commands.Context, member: discord.Member = None):
-        if not member or member == ctx.author:
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def auto_battle(self, ctx, member: discord.Member = None):
+        if member == None or member == ctx.author:
             await ctx.reply("Suicide is not an option!")
+        elif member == self.bot.user:
+            await ctx.reply("You can't beat the bot.")
         else:
-            embed = discord.Embed(
-                description=f"A challenge has been called!\n\n{ctx.author.mention} and {member.mention} are fighting to the death!"
-            )
-            msg = await ctx.reply(embed=embed)
+            msg = await ctx.reply(embed=discord.Embed(
+                title="A challenge has been called!",
+                description=f"{ctx.author.mention} and {member.mention} are fighting to the death!"
+            ))
             await asyncio.sleep(2)
-            pnts_author = 0
-            pnts_member = 0
-            for x in range(5):
+            pnts = [0, 0]
+            for x in range(6):
                 user = [ctx.author.mention, member.mention]
                 random.shuffle(user)
                 moves = [
@@ -69,23 +68,32 @@ class Fun(commands.Cog):
                     f'{user[0]} kicked {user[1]} in the legs!',
                     f'{user[0]} slapped {user[1]} in the face!',
                     f'{user[0]} shot {user[1]} with a BB gun!',
-                    f'{user[0]} hit {user[1]} in the head with a toaster!'
+                    f'{user[0]} hit {user[1]} in the head with a toaster!',
+                    f'{user[0]} makes a critical strike on {user[1]}!'
                 ]
                 if user[0] == ctx.author.mention:
-                    pnts_author = pnts_author + 1
+                    pnts[0] += 1
                 else:
-                    pnts_member = pnts_member + 1
+                    pnts[1] += 1
+                points_footer = f"{ctx.author}: {pnts[0]} points | {member}: {pnts[1]} points"
                 await msg.edit(embed=discord.Embed(
                     description=f'{random.choice(moves)}\n'
-                ).set_footer(text=f"{ctx.author}: {pnts_author} points | {member}: {pnts_member} points"))
+                ).set_footer(text=points_footer))
                 await asyncio.sleep(2)
 
-            if pnts_author == pnts_member:
-                await msg.edit(embed=discord.Embed(description="It's a tie!"))
-            elif pnts_author > pnts_member:
-                await msg.edit(embed=discord.Embed(description=f"{ctx.author.mention} wins!"))
-            elif pnts_author < pnts_member:
-                await msg.edit(embed=discord.Embed(description=f"{member.mention} wins!"))
+            if user[0] == user[1]:
+                embedmsg = discord.Embed(
+                    description="It's a tie!").set_footer(text=points_footer)
+            elif user[0] > user[1]:
+                winner = ctx.author
+                embedmsg = discord.Embed(
+                    description=f"{winner.mention} wins!").set_footer(text=points_footer)
+            elif user[0] < user[1]:
+                winner = member
+                embedmsg = discord.Embed(
+                    description=f"{winner.mention} wins!").set_footer(text=points_footer)
+
+            await msg.edit(embed=embedmsg)
 
 
 def setup(bot: commands.Bot):
