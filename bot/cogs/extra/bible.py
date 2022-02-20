274133a2da3101274293cb67ed5a9e29a0781684
get-coff3e <64768219+get-coff3e@users.noreply.github.com>
2022-02-20 02:00:08 -0600

    JESUS

--- bot/cogs/extra/bible.py
+++ bot/cogs/extra/bible.py
@@ -9,7 +9,7 @@ class Bible(commands.Cog):
         self.session = aiohttp.ClientSession()
 
     @commands.command(name="bible")
-    async def animal_fact(self, ctx: commands.Context, book: str = "Genesis", text: str = "1:1-3", trans: str = "web"):
+    async def animal_fact(self, ctx, book: str = "Genesis", text: str = "1:1-3", trans: str = "web"):
         URL = f"https://bible-api.com/{book}%20{text}?verse_numbers=true&translation={trans}"
         async with self.session.get(URL, headers={}) as response:
             if response.status == 200:
@@ -19,11 +19,16 @@ class Bible(commands.Cog):
                     url=URL,
                     description=data["text"][:750]
                 )
-                embed.set_footer(
-                    text=f"Translation: {data['translation_name']} ({data['translation_id']})")
-                await ctx.reply(embed=embed)
+                embed.set_footer(text=f"Translation: {data['translation_name']} ({data['translation_id']})")
+                msg = await ctx.reply(embed=embed)
             else:
-                await ctx.send(f"API returned a {response.status} status")
+                embed = discord.Embed(
+                    color=discord.Color.red(),
+                    description=f'Likely not a real book / verse. Try again.\n\nCheck https://bible-api.com for more details.'
+                )
+                embed.set_footer(text=f'API Response status: {response.status}')
+                await ctx.reply(embed=embed,
+                delete_after=30)
 
 
 def setup(bot: commands.Bot):
