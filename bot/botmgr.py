0b1480d1f593e39dfe9b1858243db8a46a0c9fa5
get-coff3e <64768219+get-coff3e@users.noreply.github.com>
2022-02-20 01:58:18 -0600

    less garbage + a command that doesnt work

--- bot/botmgr.py
+++ bot/botmgr.py
@@ -1,5 +1,6 @@
 import os
 import psutil
+import aiohttp
 import platform
 import nextcord
 import sysconfig
@@ -13,6 +14,33 @@ class botManagement(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.command(name='exit', aliases=['quit', 'bye', 'shutdown', 'stop'])
+    @commands.is_owner()
+    async def quit(self, ctx: commands.Context, timeset: int = None):
+        if timeset:
+            await ctx.reply(f'Stopping in {timeset} seconds.')
+            await asyncio.sleep(timeset)
+        message = 'Goodbye!'
+        try:
+            await ctx.reply(message)
+        except:
+            await ctx.author.send(message)
+        await self.bot.close()
+
+    @commands.command(name='download', aliases=['dl'])
+    @commands.is_owner()
+    async def botdl(self, ctx: commands.Context, URL: str = None, name: str = None):
+        async with self.session.get(URL, headers={}) as response:
+            if response.status == 200:
+                path = 'dl'
+                data = await response.read()
+                filename = URL.split('/')
+                with open(f"{path}/{URL[-1]}", 'w') as f:
+                    f.write(data)
+                    f.close()
+            else:
+                await ctx.reply(f"```Response from URL returned {response.status}```")
+
     @commands.command(name='dump')
     @commands.is_owner()
     async def dump(self, ctx: commands.Context, file: str = None):
@@ -20,40 +48,31 @@ class botManagement(commands.Cog):
             await ctx.reply(file=discord.File(file))
         except Exception as e:
             await ctx.reply(f'```{e}```')
-        else:
-            await ctx.author.send(file=discord.File(file))
 
     @commands.command(name='sysinfo')
     async def client_sysinfo(self, ctx: commands.Context):
-        botavatar = self.bot.user.avatar.url
-        botuser = self.bot.user
-        pyversion_imp = platform.python_implementation()
-        pyversion = platform.python_version()
-        nxtcord_ver = nextcord.__version__
-        osplatform = sysconfig.get_platform()
-        osplatform_rel = platform.release()
         # https://cog-creators.github.io/discord-embed-sandbox/
         embed = discord.Embed(
-            title='Instance information',
+            title='Instance Information',
             color=discord.Color.purple()
         )
         embed.set_thumbnail(
-            url=botavatar
+            url=self.bot.user.avatar.url
         )
         embed.add_field(
             name='Name', value=f'`{env.botname}`', inline=True
         )
         embed.add_field(
-            name='Discord User', value=f'{botuser}\n{botuser.mention}', inline=True
+            name='Discord User', value=f'{self.bot.user}\n{self.bot.user.mention}', inline=True
         )
         embed.add_field(
-            name='Python Version', value=f'`{pyversion_imp} {pyversion}`', inline=False
+            name='Python Version', value=f'`{platform.python_implementation()} {platform.python_version()}`', inline=False
         )
         embed.add_field(
-            name='Nextcord Version', value=f'`{nxtcord_ver}`', inline=True
+            name='Nextcord Version', value=f'`{nextcord.__version__}`', inline=True
         )
         embed.add_field(
-            name='OS', value=f'`{osplatform} {osplatform_rel}`', inline=False
+            name='OS', value=f'`{sysconfig.get_platform()} {platform.release()}`', inline=False
         )
         await ctx.reply(embed=embed)
 
@@ -66,27 +85,17 @@ class botManagement(commands.Cog):
     @bmg.group()
     async def guilds(self, ctx: commands.Context):
         guildlist = bot.guilds
-        try:
-            await ctx.reply(f'{guildlist}')
-        except Exception as e:
-            await ctx.reply(f'Error: ```{e}```')
+        await ctx.reply(f'{guildlist}')
 
-    @bmg.group(name='exit', aliases=['quit', 'bye', 'shutdown', 'stop'])
-    async def quit(self, ctx: commands.Context, timeset: int = None):
-        if timeset != None:
-            message = f'Stopping in {timeset} seconds.'
-            await ctx.reply(message)
-            await asyncio.sleep(timeset)
-        message = 'Goodbye!'
-        try:
-            await ctx.reply(message)
-        except:
-            await ctx.author.send(message)
-        exit()
-
-    @bmg.group(name='command', aliases=['c'])
-    async def cmdmgmt(self, ctx: commands.Context, cmd: str):
-        await ctx.reply('...')
+    @bmg.group(name='exec', aliases=['exe'])
+    async def cmdmgmt(self, ctx: commands.Context, *, cmd: str):
+        subprocesspipe = asyncio.subprocess.PIPE
+        child = await asyncio.create_subprocess_shell(
+            cmd,
+            stderr=subprocesspipe,
+            stdout=subprocesspipe
+        )
+        await ctx.reply(f"```{stdout}```")
 
 
 def setup(bot: commands.Bot):
