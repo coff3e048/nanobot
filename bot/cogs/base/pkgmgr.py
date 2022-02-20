e7ba0e65cce70fa2fdf5e30f0e1f7b1588676c2e
get-coff3e <64768219+get-coff3e@users.noreply.github.com>
2022-02-20 02:00:03 -0600

    uhhh less crap i think

--- bot/cogs/base/pkgmgr.py
+++ bot/cogs/base/pkgmgr.py
@@ -1,5 +1,6 @@
 import os
-#import aiohttp
+import aiohttp
+import aiofiles.os
 from os import getenv
 import discord
 from nextcord.ext import commands
@@ -9,70 +10,61 @@ from console import console
 class CogManager(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-
-    #async def get_cog(self, ctx):
-
+        self.session = aiohttp.ClientSession()
 
     @commands.group(name="cog", aliases=["pkg", "c"])
     @commands.is_owner()
-    async def cog(self, ctx: commands.Context, subcommand: str = 'list', cog: str = None):
-        if not ctx.invoked_subcommand:
+    async def cog(self, ctx):
+        if ctx.invoked_subcommand == None:
             await ctx.reply(f"{subcommand} is not a valid command.")
-        coglist = []
-        for cogs in self.bot.cogs:
-            coglist.append(cogs)
-        try:
-            await ctx.reply(f"```{coglist}```")
-        except Exception as e:
-            await ctx.reply(f"{e}")
 
     @cog.group(name="list")
-    async def list(self, ctx: commands.Context):
-        coglist = []
-        for cogs in self.bot.cogs:
-            cogslist.append(cogs)
-        try:
-            await ctx.reply(f"```{coglist}```")
-        except Exception as e:
-            await ctx.reply(f"{e}")
+    async def c_list(self, ctx):
+        await ctx.reply(f'```{self.bot.cogs}```')
 
     @cog.group(name="load")
-    async def load(self, ctx: commands.Context, *, cogs: str = None):
-        split_text = cogs.split()
-        for cogs in split_text:
+    async def load(self, ctx, *, cogs: str = None):
+        for cogs in cogs.split():
             try:
                 self.bot.load_extension(cogs)
-                await ctx.reply(f"{cogs} loaded.")
-            except Exception as e:
-                await ctx.reply(e)
-            
+                await ctx.reply(f"```{cogs} loaded.```")
+            except:
+                pass
+
     @cog.group(name="unload")
-    async def unload(self, ctx: commands.Context, *, cogs: str = None):
-        split_text = cogs.split()
-        for cogs in split_text:
+    async def unload(self, ctx, *, cogs: str = None):
+        for cogs in cogs.split():
             try:
                 self.bot.unload_extension(cogs)
-                await ctx.reply(f"{cogs} unloaded.")
-            except Exception as e:
-                await ctx.reply(e)
+                await ctx.reply(f"```{cogs} unloaded.```")
+            except:
+                pass
 
     @cog.group(name="reload")
-    async def reload(self, ctx: commands.Context, *, cogs: str = None):
-        split_text = cogs.split()
-        for cogs in split_text:
+    async def reload(self, ctx, *, cogs: str = None):
+        for cogs in cogs.split():
             try:
                 self.bot.reload_extension(cogs)
-                await ctx.reply(f"{cogs} reloaded.")
-            except:
+            except commands.ExtensionNotLoaded:
                 self.bot.load_extension(cogs)
+            await ctx.reply(f"```{cogs} reloaded.```")
 
     @cog.group(name="install")
-    async def install(self, ctx: commands.Context, repo: str = None, *, cogs: str = None):
-        await ctx.reply("This functionality will come soon!")
+    async def install(self, ctx, repo: str = None, *, cogs: str = None):
+        async with self.session.get(URL, headers={}) as response:
+            if response.status == 200:
+                data = await response.read()
+                filename = URL.split('/')
+                with open(URL[-1], 'w') as f:
+                    f.write(data)
+                    f.close()
+            else:
+                await ctx.reply(f"```URL ({URL}) returned {response.status}```")
 
     @cog.group(name="uninstall")
-    async def uninstall(self, ctx: commands.Context, *, cogs: str = None):
+    async def uninstall(self, ctx, *, cogs: str = None):
         await ctx.reply("This functionality will come soon!")
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(CogManager(bot))
